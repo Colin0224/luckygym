6ab0e65ac84fa9bd67ca23c639ed3f8383b346c6
LuckyHappyFish <colinwong0224@gmail.com>
2023-08-07 13:51:26 -0400

    I'm not sure

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
 import Image from 'next/image'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div>
-      </div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {
+        <h1>Gym Lucky</h1>
+      }
+    </div>
   )
 }
